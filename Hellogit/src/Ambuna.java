e270eb5df1fb7f1b74c38360de3569bbfe44ab0c
tuvshee <tuvshee@172.16.153.179>
2024-03-05 11:41:27 +0800

    oor package

--- Hellogit/src/Ambuna.java
+++ Hellogit/src/Ambuna.java
@@ -1,9 +1,9 @@
 
-public class HelloWorld {
+public class Ambuna {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("mognol");
+		System.out.println("ASDFLKJSAJDFL;KAJDFLSADFLKJSADKKKK");
 
 	}
 
